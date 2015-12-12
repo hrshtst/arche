d79e6d173f5974ff351c4d98aaeebdf30b1807c9
Hiroshi Atsuta <atsuta@vr05>
2015-12-12 14:28:25 +0900

    Add create_symlinks.sh

---
+++ create_symlinks.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eux
+
+mkdir_if_not_exist () {
+    local dir=$1
+    if [ ! -d $dir ]; then
+        mkdir -p $dir
+    fi
+}
+
+# Emacs
+mkdir_if_not_exist ~/.emacs.d
+ln -sf $PWD/emacs/el-get-recipes ~/.emacs.d/
+ln -sf $PWD/emacs/init ~/.emacs.d/
+ln -sf $PWD/emacs/init.el ~/.emacs.d/
+ln -sf $PWD/emacs/init-el-get.el ~/.emacs.d/
+ln -sf $PWD/emacs/init-loader ~/.emacs.d/
+ln -sf $PWD/emacs/snippets ~/.emacs.d/
+ln -sf $PWD/emacs/themes ~/.emacs.d/
