36c8f469a1f4a65355c47b65f90dc25d569cddc8
Hiroshi Atsuta <atsuta@vr05>
2015-12-12 14:39:37 +0900

    Add Xmodmap

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -18,3 +18,6 @@ ln -sf $PWD/emacs/init-el-get.el ~/.emacs.d/
 ln -sf $PWD/emacs/init-loader ~/.emacs.d/
 ln -sf $PWD/emacs/snippets ~/.emacs.d/
 ln -sf $PWD/emacs/themes ~/.emacs.d/
+
+# Keyboard
+ln -sf $PWD/keyboard/Xmodmap_hhk ~/.Xmodmap
