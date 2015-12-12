d520d6baf1a046dc06a25dca4921f095ecef5c83
Hiroshi Atsuta <atsuta@vr05>
2015-12-12 17:32:31 +0900

    Add configuration for bash

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -9,18 +9,38 @@ mkdir_if_not_exist () {
     fi
 }
 
+ln_if_target_exist () {
+    local target=$1
+    local link=$2
+    if [ -f $target -o -d $target ]; then
+        ln -sf $target $link
+    else
+        echo "$target doesn't exist!"
+    fi
+}
+
 # emacs
 mkdir_if_not_exist ~/.emacs.d
-ln -sf $PWD/emacs/el-get-recipes ~/.emacs.d/
-ln -sf $PWD/emacs/init ~/.emacs.d/
-ln -sf $PWD/emacs/init.el ~/.emacs.d/
-ln -sf $PWD/emacs/init-el-get.el ~/.emacs.d/
-ln -sf $PWD/emacs/init-loader ~/.emacs.d/
-ln -sf $PWD/emacs/snippets ~/.emacs.d/
-ln -sf $PWD/emacs/themes ~/.emacs.d/
+ln_if_target_exist $PWD/emacs/el-get-recipes ~/.emacs.d/
+ln_if_target_exist $PWD/emacs/init ~/.emacs.d/
+ln_if_target_exist $PWD/emacs/init.el ~/.emacs.d/
+ln_if_target_exist $PWD/emacs/init-el-get.el ~/.emacs.d/
+ln_if_target_exist $PWD/emacs/init-loader ~/.emacs.d/
+ln_if_target_exist $PWD/emacs/snippets ~/.emacs.d/
+ln_if_target_exist $PWD/emacs/themes ~/.emacs.d/
 
 # keyboard
-ln -sf $PWD/keyboard/Xmodmap_hhk ~/.Xmodmap
+ln_if_target_exist $PWD/keyboard/Xmodmap_hhk ~/.Xmodmap
+
+# bash
+ln_if_target_exist $PWD/shell/bashrc ~/.bashrc
+ln_if_target_exist $PWD/shell/bash_aliases ~/.bash_aliases
+ln_if_target_exist $PWD/shell/bash_completion_make.sh ~/.bash_completion_make.sh
 
 # tmux
-ln -sf $PWD/shell/tmux.conf ~/.tmux.conf
+ln_if_target_exist $PWD/shell/tmux.conf ~/.tmux.conf
+ln_if_target_exist ~/src/tmux/examples/bash_completion_tmux.sh ~/.bash_completion_tmux.sh
+
+# git
+ln_if_target_exist $PWD/git/gitconfig ~/.gitconfig
+ln_if_target_exist ~/src/git/contrib/completion/git-completion.bash ~/.git-completion.bash
