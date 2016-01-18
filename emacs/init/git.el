003c39b1c60788e99d4cce717457050afa445a80
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2016-01-19 06:35:10 +0900

    Fix git-gutter setting

--- emacs/init/git.el
+++ emacs/init/git.el
@@ -5,9 +5,4 @@
 ;; git-gutter
 (global-git-gutter-mode +1)
 
-(custom-set-variables
- '(git-gutter:verbosity 4)
- '(git-gutter:modified-sign " ")
- '(git-gutter:deleted-sign " "))
-
 (add-hook 'focus-in-hook 'git-gutter:update-all-windows)
