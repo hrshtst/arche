42bf8ebef9056b4d46038bc71944e30a309fd750
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2016-01-20 18:26:55 +0900

    Add key mapping

--- emacs/init/global.el
+++ emacs/init/global.el
@@ -5,6 +5,7 @@
 
 (with-eval-after-load 'helm-gtags
   (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
+  (define-key helm-gtags-mode-map (kbd "M-T") 'helm-gtags-find-tag-other-window)
   (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
   (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
   (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
