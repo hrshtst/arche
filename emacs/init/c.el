24c359fea8c3e2f6cf74914eaa720c1bbf55e78e
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2016-02-05 11:09:29 +0900

    Add setting on comment style in c mode

--- emacs/init/c.el
+++ emacs/init/c.el
@@ -4,6 +4,7 @@
   (c-set-style "k&r")
   (setq c-basic-offset 2)
   (setq indent-tabs-mode nil)
+  (setq comment-style 'indent)
   (c-toggle-electric-state -1)
   (c-toggle-auto-newline t)
   (c-toggle-hungry-state t))
