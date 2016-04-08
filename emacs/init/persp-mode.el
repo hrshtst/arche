a8c7919533ac0757a1da7f44f474cc674b1a9f50
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2016-04-08 13:47:40 +0900

    Set auto resume time for persp-mode

--- emacs/init/persp-mode.el
+++ emacs/init/persp-mode.el
@@ -1,4 +1,6 @@
 ;; persp-mode
+(custom-set-variables
+ '(persp-auto-resume-time 1.0))
 (with-eval-after-load "persp-mode"
   (setq wg-morph-on nil)
   (add-hook 'after-init-hook #'(lambda () (persp-mode 1))))
