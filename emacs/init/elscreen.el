0d330755a80d9bdd2c0150dcd133e46607a70838
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2016-04-08 15:32:20 +0900

    Add keybinds to elscreen

--- emacs/init/elscreen.el
+++ emacs/init/elscreen.el
@@ -3,6 +3,8 @@
 (global-set-key (kbd "C-z C-z") 'elscreen-toggle)
 (global-set-key (kbd "C-z .") 'elscreen-screen-nickname)
 (global-set-key (kbd "C-z C-l") 'helm-elscreen)
+(global-set-key (kbd "C-<tab>") 'elscreen-next)
+(global-set-key (kbd "<C-S-iso-lefttab>") 'elscreen-previous)
 
 (custom-set-variables
  '(elscreen-tab-display-kill-screen nil)
