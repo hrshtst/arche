9e615b85def91b0c813f487e8d3914853b9a04b2
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2016-04-08 14:02:55 +0900

    Interpret optional argument -persp-q

--- emacs/init/persp-mode.el
+++ emacs/init/persp-mode.el
@@ -1,6 +1,13 @@
 ;; persp-mode
 (custom-set-variables
  '(persp-auto-resume-time 1.0))
+
+(add-to-list 'command-switch-alist
+             (cons "persp-q"
+                   #'(lambda (p)
+                       (setq persp-auto-resume-time -1
+                             persp-auto-save-opt 0))))
+
 (with-eval-after-load "persp-mode"
   (setq wg-morph-on nil)
   (add-hook 'after-init-hook #'(lambda () (persp-mode 1))))
