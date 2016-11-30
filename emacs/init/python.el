20f01fc8c1ac154c3b3171e1ce7efe683c9360b7
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2016-11-30 11:40:40 +0900

    Fix to work jedi

--- emacs/init/python.el
+++ emacs/init/python.el
@@ -1,6 +1,23 @@
 ;; python
 (defun my/python-mode-hook ()
-  (jedi:setup))
+  ;; python common
+  (setq python-python-command "python3")
+  (setq indent-tabs-mode nil)
+  (setq indent-level 4)
+  (setq python-indent 4)
+  (setq tab-width 4)
+  ;; jedi
+  (jedi:setup)
+  (setq jedi:complete-on-dot t)
+  (setq jedi:environment-root "env")
+  (setq jedi:environment-virtualenv
+        (append python-environment-virtualenv
+                '("--python" "python3"))))
+
+(eval-after-load "jedi"
+  '(progn
+     (setq jedi:server-command
+           (list python-python-command jedi:server-script))))
 
 (with-eval-after-load 'python
   (add-hook 'python-mode-hook 'my/python-mode-hook)
@@ -13,10 +30,6 @@
   (smartrep-define-key
       python-mode-map "C-c" '(("h" . 'python-indent-shift-left)
                               ("l" . 'python-indent-shift-right))))
-
-;; jedi
-(custom-set-variables
- '(jedi:complete-on-dot t))
 
 ;; yasnippet
 (defun ac-python-mode-setup ()
