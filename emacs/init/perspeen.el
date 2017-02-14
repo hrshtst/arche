437f17eee209042f0d07c906331d115529f8f9f8
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-02-14 21:26:23 +0900

    Add some keybind for perspeen mode

--- emacs/init/perspeen.el
+++ emacs/init/perspeen.el
@@ -1,3 +1,5 @@
 (setq perspeen-keymap-prefix (kbd "C-z"))
 (define-key perspeen-mode-map (kbd "C-z SPC") 'perspeen-goto-last-ws)
 (define-key perspeen-mode-map (kbd "C-z .") 'perspeen-rename-ws)
+(define-key perspeen-mode-map (kbd "C-z h") 'perspeen-tab-prev)
+(define-key perspeen-mode-map (kbd "C-z l") 'perspeen-tab-next)
