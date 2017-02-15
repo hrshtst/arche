3cbdd7bd3af40f4f8bfd636721f89a3b2fdb34d6
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-02-15 22:02:24 +0900

    Make emacs mode line tidy up

--- emacs/init/basic_setting.el
+++ emacs/init/basic_setting.el
@@ -64,9 +64,37 @@
 (unless (server-running-p)
   (server-start))
 
-;; show in which function you are (only available in C)
-(which-function-mode +1)
-(setq-default which-func-unknown "")
+(defvar mode-line-cleaner-alist
+  '( ;; For minor-mode, first char is 'space'
+    (yas-minor-mode . " Ys")
+    (paredit-mode . " Pe")
+    (eldoc-mode . "")
+    (abbrev-mode . "")
+    (undo-tree-mode . " Ut")
+    (elisp-slime-nav-mode . " EN")
+    (helm-gtags-mode . " HG")
+    (flymake-mode . " Fm")
+    (flycheck-mode . "Fc")
+    (helm-mode . "")
+    ;; Major modes
+    (lisp-interaction-mode . "Li")
+    (python-mode . "Py")
+    (ruby-mode   . "Rb")
+    (emacs-lisp-mode . "El")
+    (markdown-mode . "Md")))
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for (mode . mode-str) in mode-line-cleaner-alist
+        do
+        (let ((old-mode-str (cdr (assq mode minor-mode-alist))))
+          (when old-mode-str
+            (setcar old-mode-str mode-str))
+          ;; major mode
+          (when (eq mode major-mode)
+            (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
 
 ;; show paren mode
 (show-paren-mode t)
