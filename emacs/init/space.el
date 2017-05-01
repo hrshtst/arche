670c7736f70c5b0e5fa6f27e138cc96b087f52e6
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-05-01 10:46:13 +0900

    Disable white space cleanup

--- emacs/init/space.el
+++ emacs/init/space.el
@@ -5,7 +5,7 @@
 (setq-default show-trailing-whitespace t)
 
 ;; clean-up whitespaces when saving a file
-(add-hook 'before-save-hook 'whitespace-cleanup)
+;; (add-hook 'before-save-hook 'whitespace-cleanup)
 ;; fix whitespace-cleanup
 ;; http://stackoverflow.com/questions/7349487/emacs-different-tab-indent-settings-in-different-modes
 (defadvice whitespace-cleanup (around whitespace-cleanup-indent-tab
