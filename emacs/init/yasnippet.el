63fc49fc6f52cc76ae40ddafc701efb99eef6cd2
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-08-07 01:50:47 +0900

    Fix problem that my snippets have not loaded

--- emacs/init/yasnippet.el
+++ emacs/init/yasnippet.el
@@ -1,3 +1,8 @@
 ;; yasnippet
+(custom-set-variables
+ '(yas-snippet-dirs (list (concat user-emacs-directory "snippets")
+                          yas-installed-snippets-dir))
+ '(yas-alias-to-yas/prefix-p nil))
+
 (with-eval-after-load 'yasnippet
   (yas-global-mode 1))
