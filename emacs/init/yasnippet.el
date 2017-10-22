705955b3363957aefbf9a4a3e74ec966e752b670
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-10-22 22:43:07 +0200

    Fix to enable yasnippet explicitly in some modes

--- emacs/init/yasnippet.el
+++ emacs/init/yasnippet.el
@@ -1,4 +1,18 @@
 ;; yasnippet
+
+;; enable yasnippet as minor mode in some specific modes
+(dolist (hook '(c-mode-hook
+                c++-mode-hook
+                emacs-lisp-mode-hook
+                html-mode-hook
+                python-mode-hook
+                go-mode-hook
+                sh-mode-hook
+                markdown-mode-hook
+                makefile-mode-hook
+                text-mode))
+  (add-hook hook 'yas-minor-mode))
+
 (custom-set-variables
  '(yas-snippet-dirs (list (concat user-emacs-directory "snippets")
                           yas-installed-snippets-dir))
@@ -6,4 +20,4 @@
 
 (with-eval-after-load 'yasnippet
   (define-key yas-keymap (kbd "<tab>") nil)
-  (yas-global-mode 1))
+  (yas-reload-all))
