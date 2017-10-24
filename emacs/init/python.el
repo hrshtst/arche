beee4de6529d897e8263c2faf72a3ca1f1c2b509
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-10-24 13:22:29 +0200

    Modify to use company when completing in python

--- emacs/init/python.el
+++ emacs/init/python.el
@@ -26,7 +26,7 @@
   (add-hook 'python-mode-hook 'my/python-mode-hook)
 
   ;; binding
-  (define-key python-mode-map (kbd "M-.") 'jedi:complete)
+  (define-key python-mode-map (kbd "C-M-i") 'company-complete)
   (define-key python-mode-map (kbd "M-t") 'jedi:goto-definition)
   (define-key python-mode-map (kbd "C-t") 'jedi:goto-definition-pop-marker)
 
