b71122d52d1997393cb29d62e4cc11ec1bb83352
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-11-08 12:11:46 +0100

    Fix shell mode

--- emacs/init/shell.el
+++ emacs/init/shell.el
@@ -1,4 +1,10 @@
 ;; shell
+(defun my/sh-mode-hook ()
+  (setq sh-basic-offset 2)
+  (setq sh-indentation 2))
+
+(add-hook 'sh-mode-hook 'my/sh-mode-hook)
+
 ;; (defun ac-sh-mode-setup ()
 ;;   (setq ac-sources (append '(ac-source-yasnippet) ac-sources)))
 ;; (add-hook 'sh-mode-hook 'ac-sh-mode-setup)
