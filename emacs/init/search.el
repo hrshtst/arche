ba194c1985b5ecbda7c94a3ce60633672242cc9a
Hiroshi Atsuta <hiroshi.atsuta@ams.eng.osaka-u.ac.jp>
2017-11-22 12:32:18 +0100

    Check if cmigemo is installed

--- emacs/init/search.el
+++ emacs/init/search.el
@@ -7,7 +7,10 @@
 (custom-set-variables
  '(anzu-mode-lighter "")
  '(anzu-deactivate-region t)
- '(anzu-use-migemo t)
  '(anzu-search-threshold 1000)
  '(anzu-replace-threshold 50)
  '(anzu-replace-to-string-separator " => "))
+
+(when (executable-find "cmigemo")
+  (custom-set-variables
+    '(anzu-use-migemo t)))
