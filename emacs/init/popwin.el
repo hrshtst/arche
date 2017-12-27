40ad07fa8804f8d93fa1e889f606d563ee183681
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-12-27 21:22:47 +0100

    Revert popwin-yatex

--- emacs/init/popwin.el
+++ emacs/init/popwin.el
@@ -15,8 +15,8 @@
 (push '(flycheck-error-list-mode :stick t) popwin:special-display-config)
 
 ;; yatex
-;; (require 'popwin-yatex)
-;; (push '("*YaTeX-typesetting*") popwin:special-display-config)
-;; (push '("*latex-math-preview-expression*") popwin:special-display-config)
+(require 'popwin-yatex)
+(push '("*YaTeX-typesetting*") popwin:special-display-config)
+(push '("*latex-math-preview-expression*") popwin:special-display-config)
 
 (setq popwin:popup-window-height 0.5)
