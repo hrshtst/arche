bf3845aabda4b424b2c85c7fd7da369f6b0cc60d
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-01-02 00:16:26 +0100

    Fix sources of company mode for c-mode

--- emacs/init/c.el
+++ emacs/init/c.el
@@ -19,6 +19,7 @@
   (setq comment-style 'indent)
   (c-toggle-auto-newline t)
   (c-toggle-hungry-state t)
+  (setq-local company-backends '((company-irony company-clang company-yasnippet company-dabbrev)))
   (clang-format-buffer-smart-on-save))
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
