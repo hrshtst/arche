70cf0183fd8182a2992d96f2e435258efab449d8
Hiroshi Atsuta <hiroshi.atsuta@ams.eng.osaka-u.ac.jp>
2018-01-24 16:53:55 +0100

    Make it possible to find tests in projectile

--- emacs/init/projectile.el
+++ emacs/init/projectile.el
@@ -1,4 +1,19 @@
 ;; setting for projectile
+(defun my/projectile-test-prefix (project-type) "test_")
+(defun my/projectile-test-prefix (project-type)
+  "Find default test files prefix based on PROJECT-TYPE."
+  (cl-flet ((prefix (&optional pfx)
+                    (projectile-project-type-attribute project-type 'test-prefix pfx)))
+    (cond
+     ((member project-type '(django python-pip python-pkg python-tox))  (prefix "test_"))
+     ((member project-type '(emacs-cask)) (prefix "test-"))
+     ((member project-type '(lein-midje)) (prefix "t_"))
+     ((member project-type '(cmake)) (prefix "test_"))
+     (t (prefix)))))
+
+(custom-set-variables
+ '(projectile-completion-system 'helm)
+ '(projectile-test-prefix-function 'my/projectile-test-prefix))
+
 (projectile-global-mode)
-(setq projectile-completion-system 'helm)
 (helm-projectile-on)
