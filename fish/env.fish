65122ed6b6aa82f20803cc920d65ee21e825f12f
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-06-14 11:16:20 +0900

    Fix fish environment setting

--- fish/env.fish
+++ fish/env.fish
@@ -1,17 +1,18 @@
 # set language
-set -x LC_ALL en_US.UTF-8
-set -x LC_CTYPE en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+set -gx LC_CTYPE en_US.UTF-8
 
 # set environment values
 prepend-to-path $HOME/.local/bin
 prepend-to-path $HOME/usr/bin
-set -x LD_LIBRARY_PATH $HOME/usr/lib:$LD_LIBRARY_PATH
+set -q LD_LIBRARY_PATH; or set -gx LD_LIBRARY_PATH /usr/local/lib
+set -gx LD_LIBRARY_PATH $HOME/usr/lib:$LD_LIBRARY_PATH
 
 # python
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
 
 # pyenv - python version management
-set -x PYENV_ROOT $HOME/.pyenv
+set -gx PYENV_ROOT $HOME/.pyenv
 prepend-to-path $PYENV_ROOT/bin $PATH
 if command -v pyenv 1>/dev/null 2>&1;
     status --is-interactive; and . (pyenv init -|psub)
@@ -20,15 +21,15 @@ end
 # pipsi - python scripting manager
 switch (hostname)
 case 'rmc-lx*'
-    set -x PIPSI_HOME /home_local/atsu_hi/pipsi
-    set -x WORKON_HOME /home_local/atsu_hi/virtualenvs
+    set -gx PIPSI_HOME /home_local/atsu_hi/pipsi
+    set -gx WORKON_HOME /home_local/atsu_hi/virtualenvs
 end
 
 # Go
-set -x GOPATH $HOME/.go:$HOME/develop/go
+set -gx GOPATH $HOME/.go:$HOME/develop/go
 prepend-to-path $HOME/.go/bin
 prepend-to-path /usr/lib/go-1.10/bin
 
 # GNU global
-set -x GTAGSLIBPATH $HOME/usr/src
+set -gx GTAGSLIBPATH $HOME/usr/src
 
