9324e9679129030730dbaad284b9bd6337a1f5f3
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2019-04-18 12:19:15 +0900

    Add new initialization file for emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,57 @@
+;;; init.el --- My personal configuration file for Emacs -*- lexical-binding: t -*-
+
+;; Copyright (c) 2016-2019 Hiroshi Atsuta
+
+;; Author: Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+
+;; This is just a configuration file for Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+;; Change working directory to HOME unless non-default initialization
+;; file is specified.
+(unless load-file-name
+  (cd (getenv "HOME")))
+
+;; If non-default initialiation file is specified, set
+;; user-emacs-directory to its directory.
+(when load-file-name
+  (setq-default user-emacs-directory
+                (file-name-directory load-file-name)))
+
+;; Make sure Emacs is running on modern version, otherwise abort.
+(let ((minimum-emacs-version "26"))
+  (when (version< emacs-version minimum-emacs-version)
+    (error "Requires at least Emacs %s, but running Emacs %s"
+           minimum-emacs-version emacs-version)))
+
+;; Prevent package.el from installing any packages
+(setq package-enable-at-startup nil)
+
+;; Prevent this file being modified by custom
+(setq custom-file (expand-file-name
+                   (format "custom-%d-%d.el" (emacs-pid) (random))
+                   temporary-file-directory))
+
+;;; init.el ends here
