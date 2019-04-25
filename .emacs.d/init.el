5dc207036fbacfb4ae413f19a60bf412ac1faa29
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2019-04-25 20:16:11 +0900

    Add installation of projectile package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -118,7 +118,10 @@
 ;; let us make them what we want. Also, this can be used to override
 ;; the definition of an internal function from another package to
 ;; make them work as desired.
-(straight-use-package 'el-patch
+(use-package el-patch
+  :straight (:host github
+                   :repo "raxod502/el-patch"
+                   :branch "develop")
   :demand t)
 
 ;;; Keybindings
@@ -215,5 +218,242 @@
 ;; `list-buffers' command.
 (use-feature ibuffer
   :bind (([remap list-buffers] . ibuffer)))
+
+;;; Finding files
+
+;; Follow symlinks when opening files.
+(setq find-file-visit-truename t)
+;; Suppress warning "X and Y are the same file".
+(setq find-file-suppress-same-file-warnings t)
+
+;; Feature `saveplace' remembers place in files so as to move point
+;; to saved position automatically when visiting them later.
+(use-feature saveplace
+  :demand t
+  :config
+  (save-place-mode +1))
+
+;; Package `projectile' provides a set of functions to operate on
+;; project level such as Git repository, CMake project, etc.
+;; (use-package projectile
+;;   :init
+;;   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+;;   (projectile-mode +1)
+;;   :defer 1
+;;   :config
+;;   :blackout t)
+
+(use-package projectile
+  :init/el-patch
+
+  (defcustom projectile-keymap-prefix nil
+    "Projectile keymap prefix."
+    :group 'projectile
+    :type 'string)
+
+  (defvar projectile-command-map
+    (let ((map (make-sparse-keymap)))
+      (define-key map (kbd "4 a") #'projectile-find-other-file-other-window)
+      (define-key map (kbd "4 b") #'projectile-switch-to-buffer-other-window)
+      (define-key map (kbd "4 C-o") #'projectile-display-buffer)
+      (define-key map (kbd "4 d") #'projectile-find-dir-other-window)
+      (define-key map (kbd "4 D") #'projectile-dired-other-window)
+      (define-key map (kbd "4 f") #'projectile-find-file-other-window)
+      (define-key map (kbd "4 g") #'projectile-find-file-dwim-other-window)
+      (define-key map (kbd "4 t") #'projectile-find-implementation-or-test-other-window)
+      (define-key map (kbd "5 a") #'projectile-find-other-file-other-frame)
+      (define-key map (kbd "5 b") #'projectile-switch-to-buffer-other-frame)
+      (define-key map (kbd "5 d") #'projectile-find-dir-other-frame)
+      (define-key map (kbd "5 D") #'projectile-dired-other-frame)
+      (define-key map (kbd "5 f") #'projectile-find-file-other-frame)
+      (define-key map (kbd "5 g") #'projectile-find-file-dwim-other-frame)
+      (define-key map (kbd "5 t") #'projectile-find-implementation-or-test-other-frame)
+      (define-key map (kbd "!") #'projectile-run-shell-command-in-root)
+      (define-key map (kbd "&") #'projectile-run-async-shell-command-in-root)
+      (define-key map (kbd "a") #'projectile-find-other-file)
+      (define-key map (kbd "b") #'projectile-switch-to-buffer)
+      (define-key map (kbd "C") #'projectile-configure-project)
+      (define-key map (kbd "c") #'projectile-compile-project)
+      (define-key map (kbd "d") #'projectile-find-dir)
+      (define-key map (kbd "D") #'projectile-dired)
+      (define-key map (kbd "e") #'projectile-recentf)
+      (define-key map (kbd "E") #'projectile-edit-dir-locals)
+      (define-key map (kbd "f") #'projectile-find-file)
+      (define-key map (kbd "g") #'projectile-find-file-dwim)
+      (define-key map (kbd "F") #'projectile-find-file-in-known-projects)
+      (define-key map (kbd "i") #'projectile-invalidate-cache)
+      (define-key map (kbd "I") #'projectile-ibuffer)
+      (define-key map (kbd "j") #'projectile-find-tag)
+      (define-key map (kbd "k") #'projectile-kill-buffers)
+      (define-key map (kbd "l") #'projectile-find-file-in-directory)
+      (define-key map (kbd "m") #'projectile-commander)
+      (define-key map (kbd "o") #'projectile-multi-occur)
+      (define-key map (kbd "p") #'projectile-switch-project)
+      (define-key map (kbd "q") #'projectile-switch-open-project)
+      (define-key map (kbd "P") #'projectile-test-project)
+      (define-key map (kbd "r") #'projectile-replace)
+      (define-key map (kbd "R") #'projectile-regenerate-tags)
+      (define-key map (kbd "s g") #'projectile-grep)
+      (define-key map (kbd "s r") #'projectile-ripgrep)
+      (define-key map (kbd "s s") #'projectile-ag)
+      (define-key map (kbd "S") #'projectile-save-project-buffers)
+      (define-key map (kbd "t") #'projectile-toggle-between-implementation-and-test)
+      (define-key map (kbd "T") #'projectile-find-test-file)
+      (define-key map (kbd "u") #'projectile-run-project)
+      (define-key map (kbd "v") #'projectile-vc)
+      (define-key map (kbd "V") #'projectile-browse-dirty-projects)
+      (define-key map (kbd "x e") #'projectile-run-eshell)
+      (define-key map (kbd "x i") #'projectile-run-ielm)
+      (define-key map (kbd "x t") #'projectile-run-term)
+      (define-key map (kbd "x s") #'projectile-run-shell)
+      (define-key map (kbd "z") #'projectile-cache-current-file)
+      (define-key map (kbd "<left>") #'projectile-previous-project-buffer)
+      (define-key map (kbd "<right>") #'projectile-next-project-buffer)
+      (define-key map (kbd "ESC") #'projectile-project-buffers-other-buffer)
+      map)
+    "Keymap for Projectile commands after `projectile-keymap-prefix'.")
+  (fset 'projectile-command-map projectile-command-map)
+
+  (defvar projectile-mode-map
+    (let ((map (make-sparse-keymap)))
+      (when projectile-keymap-prefix
+        (define-key map projectile-keymap-prefix 'projectile-command-map))
+      (easy-menu-define projectile-mode-menu map
+        "Menu for Projectile"
+        '("Projectile"
+          ["Find file" projectile-find-file]
+          ["Find file in known projects" projectile-find-file-in-known-projects]
+          ["Find test file" projectile-find-test-file]
+          ["Find directory" projectile-find-dir]
+          ["Find file in directory" projectile-find-file-in-directory]
+          ["Find other file" projectile-find-other-file]
+          ["Switch to buffer" projectile-switch-to-buffer]
+          ["Jump between implementation file and test file" projectile-toggle-between-implementation-and-test]
+          ["Kill project buffers" projectile-kill-buffers]
+          ["Save project buffers" projectile-save-project-buffers]
+          ["Recent files" projectile-recentf]
+          ["Previous buffer" projectile-previous-project-buffer]
+          ["Next buffer" projectile-next-project-buffer]
+          "--"
+          ["Toggle project wide read-only" projectile-toggle-project-read-only]
+          ["Edit .dir-locals.el" projectile-edit-dir-locals]
+          "--"
+          ["Switch to project" projectile-switch-project]
+          ["Switch to open project" projectile-switch-open-project]
+          ["Discover projects in directory" projectile-discover-projects-in-directory]
+          ["Browse dirty projects" projectile-browse-dirty-projects]
+          ["Open project in dired" projectile-dired]
+          "--"
+          ["Search in project (grep)" projectile-grep]
+          ["Search in project (ag)" projectile-ag]
+          ["Replace in project" projectile-replace]
+          ["Multi-occur in project" projectile-multi-occur]
+          "--"
+          ["Run shell" projectile-run-shell]
+          ["Run eshell" projectile-run-eshell]
+          ["Run ielm" projectile-run-ielm]
+          ["Run term" projectile-run-term]
+          "--"
+          ["Cache current file" projectile-cache-current-file]
+          ["Invalidate cache" projectile-invalidate-cache]
+          ["Regenerate [e|g]tags" projectile-regenerate-tags]
+          "--"
+          ["Configure project" projectile-configure-project]
+          ["Compile project" projectile-compile-project]
+          ["Test project" projectile-test-project]
+          ["Run project" projectile-run-project]
+          ["Repeat last external command" projectile-repeat-last-command]
+          "--"
+          ["Project info" projectile-project-info]
+          ["About" projectile-version]))
+      map)
+    "Keymap for Projectile mode.")
+
+  :init
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+  (projectile-mode +1)
+
+  :defer 1
+  :config
+  (projectile-mode +1)
+
+  (defun radian--projectile-indexing-method-p (method)
+    "Non-nil if METHOD is a safe value for `projectile-indexing-method'."
+    (memq method '(native alien)))
+
+  (put 'projectile-indexing-method 'safe-local-variable
+       #'radian--projectile-indexing-method-p)
+
+  :blackout t)
+
+;; Package `counsel-projectile' provides alternate versions of
+;; Projectile commands which use Counsel.
+(use-package counsel-projectile
+  :init/el-patch
+
+  (defcustom counsel-projectile-key-bindings
+    '((projectile-find-file        . counsel-projectile-find-file)
+      (projectile-find-file-dwim   . counsel-projectile-find-file-dwim)
+      (projectile-find-dir         . counsel-projectile-find-dir)
+      (projectile-switch-to-buffer . counsel-projectile-switch-to-buffer)
+      (projectile-grep             . counsel-projectile-grep)
+      (projectile-ag               . counsel-projectile-ag)
+      (projectile-ripgrep          . counsel-projectile-rg)
+      (projectile-switch-project   . counsel-projectile-switch-project)
+      (" "                         . counsel-projectile)
+      ("si"                        . counsel-projectile-git-grep)
+      ("Oc"                        . counsel-projectile-org-capture)
+      ("Oa"                        . counsel-projectile-org-agenda))
+    "Alist of counsel-projectile key bindings.
+
+Each element is of the form \(KEY . DEF\) where KEY is either a
+key sequence to bind in `projectile-command-map' or a projectile
+command to remap in `projectile-mode-map', and DEF is the
+counsel-projectile command to which KEY is remapped or bound."
+    :type '(alist :key-type (choice (function :tag "Projectile command")
+                                    key-sequence)
+                  :value-type (function :tag "Counsel-projectile command"))
+    :group 'counsel-projectile)
+
+  (define-minor-mode counsel-projectile-mode
+    "Toggle Counsel-Projectile mode on or off.
+
+With a prefix argument ARG, enable the mode if ARG is positive,
+and disable it otherwise.  If called from Lisp, enable the mode
+if ARG is omitted or nil, and toggle it if ARG is `toggle'.
+
+Counsel-Projectile mode turns on Projectile mode, thus enabling
+all projectile key bindings, and adds the counsel-projectile key
+bindings on top of them.
+
+The counsel-projectile key bindings either remap existing
+projectile commands to their counsel-projectile replacements or
+bind keys to counsel-projectile commands that have no projectile
+counterparts."
+    :group 'counsel-projectile
+    :require 'counsel-projectile
+    :global t
+    (cond
+     (counsel-projectile-mode
+      (projectile-mode)
+      (dolist (binding counsel-projectile-key-bindings)
+        (if (functionp (car binding))
+            (define-key projectile-mode-map `[remap ,(car binding)] (cdr binding))
+          (define-key projectile-command-map (car binding) (cdr binding)))))
+     (t
+      (dolist (binding counsel-projectile-key-bindings)
+        (if (functionp (car binding))
+            (define-key projectile-mode-map `[remap ,(car binding)] nil)
+          (define-key projectile-command-map (car binding) nil)))
+      (projectile-mode -1))))
+
+  :init
+
+  (counsel-projectile-mode +1)
+
+  :config
+
+  (setq counsel-projectile-sort-files t))
 
 ;;; init.el ends here
