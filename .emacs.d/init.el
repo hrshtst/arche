9a9754803bac815f5e352cc051bece0afca05966
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2019-06-14 16:41:16 +0900

    Move keybind definition

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1568,38 +1568,7 @@ via return key."
   :config
 
   ;; Use Flycheck, not Flymake.
-  (setq lsp-prefer-flymake nil)
-
-  ;; Define keybindings for lsp mode with hydra.
-  (use-feature hydra
-    :config
-
-    (defhydra hydra-lsp-mode (:exit t :hint nil)
-      "
- Buffer^^               Server^^                   Symbol
--------------------------------------------------------------------------------------
- [_f_] format           [_M-r_] restart            [_d_] declaration  [_i_] implementation  [_o_] documentation
- [_m_] imenu            [_S_]   shutdown           [_D_] definition   [_t_] type            [_r_] rename
- [_x_] execute action   [_M-s_] describe session   [_R_] references   [_s_] signature
-"
-      ("d" lsp-find-declaration)
-      ("D" lsp-ui-peek-find-definitions)
-      ("R" lsp-ui-peek-find-references)
-      ("i" lsp-ui-peek-find-implementation)
-      ("t" lsp-find-type-definition)
-      ("s" lsp-signature-help)
-      ("o" lsp-describe-thing-at-point)
-      ("r" lsp-rename)
-
-      ("f" lsp-format-buffer)
-      ("m" lsp-ui-imenu)
-      ("x" lsp-execute-code-action)
-
-      ("M-s" lsp-describe-session)
-      ("M-r" lsp-restart-workspace)
-      ("S" lsp-shutdown-workspace))
-
-    (bind-key "<f9>" #'hydra-lsp-mode/body lsp-mode-map)))
+  (setq lsp-prefer-flymake nil))
 
 ;;;; Indentation
 
@@ -1868,6 +1837,37 @@ nor requires Flycheck to be loaded."
       (apply orig-fun args)))
 
   (setq lsp-ui-sideline-ignore-duplicate t))
+
+;; Define keybindings for lsp mode with hydra.
+(use-feature hydra
+  :config
+
+  (defhydra hydra-lsp-mode (:exit t :hint nil)
+    "
+ Buffer^^               Server^^                   Symbol
+-------------------------------------------------------------------------------------
+ [_f_] format           [_M-r_] restart            [_d_] declaration  [_i_] implementation  [_o_] documentation
+ [_m_] imenu            [_S_]   shutdown           [_D_] definition   [_t_] type            [_r_] rename
+ [_x_] execute action   [_M-s_] describe session   [_R_] references   [_s_] signature
+"
+    ("d" lsp-find-declaration)
+    ("D" lsp-ui-peek-find-definitions)
+    ("R" lsp-ui-peek-find-references)
+    ("i" lsp-ui-peek-find-implementation)
+    ("t" lsp-find-type-definition)
+    ("s" lsp-signature-help)
+    ("o" lsp-describe-thing-at-point)
+    ("r" lsp-rename)
+
+    ("f" lsp-format-buffer)
+    ("m" lsp-ui-imenu)
+    ("x" lsp-execute-code-action)
+
+    ("M-s" lsp-describe-session)
+    ("M-r" lsp-restart-workspace)
+    ("S" lsp-shutdown-workspace))
+
+  (bind-key "<f9>" #'hydra-lsp-mode/body lsp-mode-map))
 
 ;;; Language support
 ;;;; Plain text
