d6e7df2fd8a83b904fe91561b592a2da50ab0f2a
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2020-09-16 15:22:07 +0900

    Add version file of packages for Emacs

---
+++ emacs/versions.el
@@ -0,0 +1,13 @@
+(("blackout" . "d99ab1aad84e4bd2bd4a499877764c213cce50ff")
+ ("el-patch" . "ff1951d776f80d2fd4a0cd9a0c930182fbb57b3c")
+ ("emacs-which-key" . "ae59b7edb0d82aa0251803fdfbde6b865083c8b8")
+ ("emacsmirror-mirror" . "a2cbb1e390e26f6fb01a256ea2812bdbc6fd9071")
+ ("gnu-elpa-mirror" . "87d1146e3dbbe05ade60eb265bf50490aecbe1e0")
+ ("melpa" . "e38a261ab64a1ce897d997fcfba6f01e9ad969f0")
+ ("no-littering" . "cd32b5e692cfba27cbd9dce22a9014141b637e1e")
+ ("org" . "10f490203082082cf9504fbd12a58c50cc170ee9")
+ ("prescient.el" . "cc289ba3b0d89f251267ca2b669d01b3afecc530")
+ ("selectrum" . "5f35759344963e7e994196c9d7595372cd13ad95")
+ ("straight.el" . "65e0f71b6fb15c9fba651bd083ece813802b8941")
+ ("use-package" . "4fb1f9a68f1e7e7d614652afc017a6652fd029f1"))
+:beta
