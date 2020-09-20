198d494b44ea4588c3cecf25ddb2a5d57d7b33a3
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2020-09-20 12:41:26 +0900

    Fix script for byte compilation

--- scripts/byte-compile.bash
+++ scripts/byte-compile.bash
@@ -3,11 +3,10 @@
 set -e
 set -o pipefail
 
-EMACS=$HOME/usr/src/emacs-27.1/src/emacs
-($EMACS --batch \
-        --eval "(setq straight-safe-mode t)"                 \
-        --load "$HOME/.emacs.d/init.el"                      \
-        --funcall arche-batch-byte-compile 2>&1              \
+(emacs --batch \
+       --eval "(setq straight-safe-mode t)"                  \
+       --load "$HOME/.emacs.d/init.el"                       \
+       --funcall arche-batch-byte-compile 2>&1               \
      | (grep -v "In toplevel form"                  || true) \
      | (grep -v "In end of data"                    || true) \
      | (grep -v "Warning: Package cl is deprecated" || true) \
