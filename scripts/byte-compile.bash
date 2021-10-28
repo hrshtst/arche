5896ad4ce2a1dac32fa47e7b449ed0ae87ebb9b5
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-10-28 21:30:31 +0900

    Temporarily ignore a specific warning

--- scripts/byte-compile.bash
+++ scripts/byte-compile.bash
@@ -10,4 +10,5 @@ set -o pipefail
      | (grep -v "In toplevel form"                  || true) \
      | (grep -v "In end of data"                    || true) \
      | (grep -v "Warning: Package cl is deprecated" || true) \
+     | (grep -v "Warning: docstring wider than 80 characters" || true) \
      | (! grep .)) || (rm -f emacs/arche.elc; false)
