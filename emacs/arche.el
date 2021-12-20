e56827a5f31a88fb54b07d5751134919ad852c5d
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-12-20 22:59:44 +0900

    Enable completion by consult with completion-in-region

--- emacs/arche.el
+++ emacs/arche.el
@@ -1066,6 +1066,15 @@ ourselves."
   (autoload 'projectile-project-root "projectile")
   (setq consult-project-root-function #'projectile-project-root)
 
+  ;; Use `consult-completion-in-region' if Vertico is enabled.
+  ;; Otherwise use the default `completion--in-region' function.
+  (setq completion-in-region-function
+        (lambda (&rest args)
+          (apply (if vertico-mode
+                     #'consult-completion-in-region
+                   #'completion--in-region)
+                 args)))
+
   :blackout t)
 
 ;; Feature `embark-consult' is included in the package Embark, which
