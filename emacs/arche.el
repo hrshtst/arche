deee67e150c000510c233e6635a5ec22c1112df2
Hiroshi Atsuta <atsuta@ieee.org>
2022-10-12 19:12:41 +0900

    Fix lsp configuration for pyright as suggested

--- emacs/arche.el
+++ emacs/arche.el
@@ -3508,7 +3508,7 @@ killed (which happens during Emacs shutdown)."
   (arche-defadvice arche--lsp-multi-root-fix (&rest _)
     :before #'lsp
     "Fix multi-root servers for `lsp-mode'."
-    (setf (lsp-session-server-id->folders (lsp-session)) (ht)))
+    (eval '(setf (lsp-session-server-id->folders (lsp-session)) (ht))))
 
   :blackout " LSP")
 
