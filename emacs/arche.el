6363b9588361e63a2071e9303bb6b8faa1850835
Radon Rosborough <radon@intuitiveexplanations.com>
2023-04-21 16:12:58 -0700

    Update some emacsql related packages
    Closes #523

--- emacs/arche.el
+++ emacs/arche.el
@@ -6281,7 +6281,8 @@ This prevents having EmacSQL try to build its binary (which may
 be annoying, inconvenient, or impossible depending on the
 situation) just because you tried to do literally anything with
 Magit."
-    (file-executable-p emacsql-sqlite-executable))
+    (and (featurep 'emacsql-sqlite)
+         (file-executable-p emacsql-sqlite-executable)))
 
   (arche-defadvice arche--forge-build-binary-lazily (&rest _)
     :before #'forge-dispatch
@@ -6290,6 +6291,7 @@ Normally, the binary gets built as soon as Forge is loaded, which
 is terrible UX. We disable that above, so we now have to manually
 make sure it does get built when we actually issue a Forge
 command."
+    (require 'emacsql-sqlite)
     (unless (file-executable-p emacsql-sqlite-executable)
       (emacsql-sqlite-compile 2))))
 
