358025318d2b332bc6b8d63c2e72f6e9d60ecf93
Hiroshi Atsuta <atsuta@ieee.org>
2024-06-12 15:19:11 +0900

    Remove my hack on restart-emacs

--- emacs/arche.el
+++ emacs/arche.el
@@ -7295,9 +7295,8 @@ spam. This advice, however, inhibits the message for everyone.")
 ;; Package `restart-emacs' provides an easy way to restart Emacs from
 ;; inside of Emacs, both in the terminal and in windowed mode.
 (use-package restart-emacs
-  :commands restart-emacs-restart
+  :commands (arche-new-emacs)
   :init
-
   (defvar arche--restart-in-progress nil
     "Used to prevent infinite recursion.
 This is non-nil if `arche--advice-kill-emacs-dispatch' has called
@@ -7308,8 +7307,7 @@ This is non-nil if `arche--advice-kill-emacs-dispatch' has called
     ;; slow (double-digit milliseconds).
     '(prescient--save
       arche--org-clock-save
-      save-place-kill-emacs-hook
-      recentf-save-list)
+      save-place-kill-emacs-hook)
     "List of functions on `kill-emacs-hook' which can be run eagerly.
 If actually present on `kill-emacs-hook', then these functions
 are run immediately on `save-buffers-kill-emacs'. This means that
@@ -7374,54 +7372,14 @@ bound dynamically before being used.")
                 ((or ?y ?Y) (funcall save-buffers-kill-emacs arg))
                 ((or ?n ?N))
                 ((or ?r ?R)
-                 (restart-emacs-restart arg))
+                 (restart-emacs arg))
                 ((or ?e ?E)
                  (restart-emacs-start-new-emacs
                   (restart-emacs--translate-prefix-to-args arg)))
                 ((or ?k ?K) (arche-really-kill-emacs))
                 (?\C-g (signal 'quit nil))
                 (_ (setq key nil))))))
-        (message "%s%c" prompt key))))
-
-  :config/el-patch
-
-  (defun (el-patch-swap restart-emacs restart-emacs-restart) (&optional args)
-    "Restart Emacs.
-
-When called interactively ARGS is interpreted as follows
-
-- with a single `universal-argument' (`C-u') Emacs is restarted
-  with `--debug-init' flag
-- with two `universal-argument' (`C-u') Emacs is restarted with
-  `-Q' flag
-- with three `universal-argument' (`C-u') the user prompted for
-  the arguments
-
-When called non-interactively ARGS should be a list of arguments
-with which Emacs should be restarted."
-    (interactive "P")
-    ;; Do not trigger a restart unless we are sure, we can restart emacs
-    (restart-emacs--ensure-can-restart)
-    ;; We need the new emacs to be spawned after all kill-emacs-hooks
-    ;; have been processed and there is nothing interesting left
-    (let* ((default-directory (restart-emacs--guess-startup-directory))
-           (translated-args (if (called-interactively-p 'any)
-                                (restart-emacs--translate-prefix-to-args args)
-                              args))
-           (restart-args (append translated-args
-                                 ;; When Emacs is started with a -Q
-                                 ;; restart-emacs's autoloads would not be present
-                                 ;; causing the the --restart-emacs-desktop
-                                 ;; argument to be unhandled
-                                 (unless (member "-Q" translated-args)
-                                   (restart-emacs--frame-restore-args))))
-           (kill-emacs-hook (append kill-emacs-hook
-                                    (unless restart-emacs--inhibit-kill-p
-                                      (list (apply-partially #'restart-emacs--launch-other-emacs
-                                                             restart-args))))))
-      (if restart-emacs--inhibit-kill-p
-          (restart-emacs--launch-other-emacs restart-args)
-        (save-buffers-kill-emacs)))))
+        (message "%s%c" prompt key)))))
 
 ;;; Miscellaneous
 
